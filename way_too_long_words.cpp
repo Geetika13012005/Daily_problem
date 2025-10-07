edfbb4be6d3edac8419a1d6e659d6e973771e30c
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-10-07 15:43:33 +0530

    Word

---
+++ way_too_long_words.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    int n;
+    cin>>n;
+
+    while (n--){
+        string word;
+        cin>>word;
+        size_t len =word.length();
+        if(len>10){
+            cout<<word[0]<<(len-2)<<word[len-1]<<'\n';
+
+            
+        }
+        else{
+            cout<<word<<'\n';
+        }
+    }
+    return 0;
+}
