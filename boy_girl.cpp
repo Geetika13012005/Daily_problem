04aff9c2f5368d6d51406e74b12d7bdbf9d43700
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-10-16 20:59:04 +0530

    BOY_GIRL

--- boy_girl.cpp
+++ boy_girl.cpp
@@ -1,25 +1,25 @@
-#include<iostream>
-#include<string>
-#include<set>
-
+#include <iostream>
+#include <string>
+#include <set>
 using namespace std;
-int main (){
-    string n;
-    cin>>n;
+
+int main() {
+    string username;
+    cin >> username;
+
     
-    set<char>uniquechar;
-    for(char c:n){
-        uniquechar.insert(c);
+    set<char> distinct_chars;
+
+    for (char c : username) {
+        distinct_chars.insert(c);
     }
 
-    
-   
-    if (uniquechar.size() %2==0)
-{
-    cout<<"CHAT WITH HER" <<endl;
+    int count = distinct_chars.size();
 
-}
-  else 
-   cout<<"IGNORE HIM! "<<endl;
-   return 0;
+    if (count % 2 == 0)
+        cout << "CHAT WITH HER!" << endl;
+    else
+        cout << "IGNORE HIM!" << endl;
+
+    return 0;
 }
