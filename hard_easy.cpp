3a40b624ea8a8f1729d156d7aff93fe7334d3b99
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-10-18 22:18:32 +0530

    problem level

---
+++ hard_easy.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main (){
+    int n;
+    cin>>n;
+
+    int opinion ;
+    bool hard=false;
+    for(int i=0;i<n;i++){
+        cin>>opinion;
+        if(opinion==1){
+            hard=true;
+        }
+    }
+    if(hard)
+    cout<<"Hard"<<endl;
+    else
+    cout<<"Easy"<<endl;
+
+    return 0;
+}
