f8c536cd77861f5a5299715466f8e6480f05143b
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-10-22 18:24:15 +0530

    polyhedron

---
+++ anton_polyhedron.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main (){
+    int n;
+    cin>>n;
+    int totalcount=0;
+
+    string s;
+    while(n--){
+     cin>>s;
+     if(s=="Tetrahedron"){
+        totalcount+=4;
+     }
+     else if(s=="cube"){
+        totalcount+=6;
+     }
+     else if(s=="Octahedron"){
+        totalcount+=8;
+     }
+     else if(s=="Dodecahedron"){
+        totalcount+=12;
+
+     }
+     else if(s=="Icosahedron"){
+        totalcount+=20;
+     }
+    }
+    cout<<totalcount<<endl;
+    return 0;
+    
+    
+}
