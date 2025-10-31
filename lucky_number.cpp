e8db0653c1b8e20f39b7059153fe7dac964aebf2
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-10-31 17:32:51 +0530

    lucky_number

---
+++ lucky_number.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    bool lucky = false;
+
+    int luckyNumbers[] = {4, 7, 44, 47, 74, 77, 444, 447, 474, 477, 744, 747, 774, 777};
+
+    for (int i = 0; i < 14; i++) {
+        if (n % luckyNumbers[i] == 0) {
+            lucky = true;
+            break;
+        }
+    }
+
+    if (lucky)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+
+    return 0;
+}
