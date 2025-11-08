83cbc25b2a677ee5008b9f09345a61c62fbc9894
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-11-08 15:48:16 +0530

    all

--- zigzag.cpp
+++ zigzag.cpp
@@ -1,40 +1,37 @@
 #include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-
-    int t;
-    cin >> t;
+    int t; 
+    if (!(cin >> t)) return 0;
     while (t--) {
-        int n;
-        cin >> n;
-        vector<long long> a(n);
-        for (int i = 0; i < n; i++) cin >> a[i];
+        int n; cin >> n;
+        vector<ll> a(n), p(n);
+        for (int i = 0; i < n; ++i) cin >> a[i];
+        // prefix max
+        p[0] = a[0];
+        for (int i = 1; i < n; ++i) p[i] = max(p[i-1], a[i]);
 
-        auto cost = [&](int start) {
-            long long ans = 0;
+        const ll INF = (ll)9e18;
+        auto compute = [&](int start)->ll {
+            // start = 0 => valleys at indices 0,2,4,...
+            // start = 1 => valleys at indices 1,3,5,...
+            ll cost = 0;
             for (int i = start; i < n; i += 2) {
-                long long left = (i > 0 ? a[i - 1] : LLONG_MAX);
-                long long right = (i + 1 < n ? a[i + 1] : LLONG_MAX);
-                long long limit = min(left, right);
-                if (a[i] >= limit) ans += (a[i] - limit + 1);
+                ll left  = (i > 0 ? /*peak/neighbor*/ ( ( (i-1) % 2 != start ) ? p[i-1] : a[i-1] ) : INF);
+                ll right = (i+1 < n ? ( ( (i+1) % 2 != start ) ? p[i+1] : a[i+1] ) : INF);
+                // we used p[] for neighbors that are peaks; valleys keep original a[]
+                ll limit = min(left, right);
+                if (a[i] >= limit) cost += (a[i] - limit + 1);
             }
-            return ans;
+            return cost;
         };
 
-        long long answer;
-        if (n % 2 == 1) {
-           
-            answer = cost(1);
-        } else {
-    
-            answer = min(cost(0), cost(1));
-        }
-
-        cout << answer << "\n";
+        // Try both starts and take min
+        cout << min(compute(0), compute(1)) << '\n';
     }
-
     return 0;
 }
