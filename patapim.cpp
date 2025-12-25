270b7d723cd870dd6771cd2656aa9b84b9148092
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2025-12-25 13:59:38 +0530

    patapim

--- patapim.cpp
+++ patapim.cpp
@@ -1,9 +1,41 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
+ 
 int main() {
-    int x;
-    cin >> x;
-    cout << x;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+ 
+    int t;
+    cin >> t;
+ 
+    while (t--) {
+        int n;
+        cin >> n;
+ 
+        vector<int> p(2 * n + 1, 0);
+        vector<bool> used(2 * n + 1, false);
+ 
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                int x;
+                cin >> x;
+                p[i + j] = x;
+                used[x] = true;
+            }
+        }
+ 
+        for (int i = 1; i <= 2 * n; i++) {
+            if (!used[i]) {
+                p[1] = i;
+                break;
+            }
+        }
+ 
+        for (int i = 1; i <= 2 * n; i++) {
+            cout << p[i] << " ";
+        }
+        cout << "\n";
+    }
+ 
+    return 0;
 }
-
