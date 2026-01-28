5007103bca3be50028430e70ff8b189b55ea2928
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-01-28 20:45:23 +0530

    perpendicularSegments

---
+++ perpendicularSegments.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int X, Y, K;
+        cin >> X >> Y >> K;
+
+        cout << 0 << " " << 0 << " " << X << " " << Y << "\n";
+
+        cout << 0 << " " << Y << " " << Y << " " << 0 << "\n";
+    }
+    return 0;
+}
