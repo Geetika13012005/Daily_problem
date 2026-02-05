72afa90f0fbaee6280997590c5b20e0df6ad3d1d
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-02-05 14:29:05 +0530

    update

--- repeatName.cpp
+++ repeatName.cpp
@@ -1,19 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main (){
+
+int main(){
     int n;
-    cin>>n;
-    unordered_map<string, int > mp;
-    while(n--){
-        string s;
-        cin>>s;
-        if(mp[s]==0){
-            cout<<"ok\n";
-            mp[s]=1;
-        }else{
-            cout<<s<<mp[s]<<"\n";
+    cin >> n;
+
+    vector<string> input(n);
+    for(int i = 0; i < n; i++){
+        
+        cin >> input[i];
+    }
+
+    unordered_map<string, int> mp;
+    vector<string> result;
+
+    // process all inputs
+    for(int i = 0; i < n; i++){
+        string s = input[i];
+
+        if(mp[s] == 0){
+            result.push_back("OK");
+            mp[s] = 1;
+        } else {
+            result.push_back(s + to_string(mp[s]));
             mp[s]++;
         }
     }
+
+    // print all outputs together
+    for(auto &res : result){
+        cout << res << "\n";
+    }
+
     return 0;
 }
