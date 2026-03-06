373d505af80446cb2023fee851ac8d9fb3a535ff
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-03-06 14:41:34 +0530

    painting

--- painting.cpp
+++ painting.cpp
@@ -1,34 +1,24 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-
+int main(){
     int t;
     cin >> t;
 
-    while (t--) {
-        int n;
-        cin >> n;
+    vector<string> ans;
 
-        set<long long> s;
-        for (int i = 0; i < n; i++) {
-            long long x;
-            cin >> x;
-            s.insert(x);
-        }
+    while(t--){
+        long long n, a, b;
+        cin >> n >> a >> b;
 
-        long long k = s.size();
-
-        // Find smallest x >= k that already exists
-        long long ans = k;
-        while (s.find(ans) == s.end()) {
-            ans++;
-        }
-
-        cout << ans << "\n";
+        if(a == n && b % 2 == 1)
+            ans.push_back("NO");
+        else
+            ans.push_back("YES");
     }
+
+    for(string x : ans)
+        cout << x << endl;
 
     return 0;
 }
