7e01708d6892829c5ef04e20943e538f3c2a827f
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-04-02 12:07:17 +0530

    coloredMarble

--- colored_marble.cpp
+++ colored_marble.cpp
@@ -7,16 +7,17 @@ int main() {
 
     int t;
     cin >> t;
+
     while (t--) {
         int n;
         cin >> n;
 
+        vector<int> a(n);
         unordered_map<int, int> freq;
 
         for (int i = 0; i < n; i++) {
-            int x;
-            cin >> x;
-            freq[x]++;
+            cin >> a[i];
+            freq[a[i]]++;
         }
 
         int cnt1 = 0, cnt2 = 0;
@@ -26,17 +27,10 @@ int main() {
             else cnt2++;
         }
 
-        int ans = 0;
-
-        // from freq = 1
-        ans += 2 * ((cnt1 + 1) / 2);
-
-        // at least one from freq >= 2
-        ans += cnt2;
-
-        // full colors from freq >= 2
-        ans += max(0, cnt2 - (cnt1 % 2));
+        int ans = cnt2 + 2 * ((cnt1 + 1) / 2);
 
         cout << ans << "\n";
     }
+
+    return 0;
 }
