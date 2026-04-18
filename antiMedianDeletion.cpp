157cdc28c5aa5fa0534ac6ddac0bfa96e05c3193
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-04-18 11:30:56 +0530

    update

--- antiMedianDeletion.cpp
+++ antiMedianDeletion.cpp
@@ -13,9 +13,7 @@ int main(){
         cin >> n;
 
         vector<int> p(n);
-        for(int i = 0; i < n; i++){
-            cin >> p[i];
-        }
+        for(int i = 0; i < n; i++) cin >> p[i];
 
         if(n == 1){
             cout << 1 << "\n";
@@ -23,17 +21,7 @@ int main(){
         }
 
         for(int i = 0; i < n; i++){
-            int L = p[i] - 1;
-            int R = n - p[i];
-
-            int k = min(L, R);
-            int ans = 1 + 2 * k;
-
-            if(L == R) ans--;   
-            
-            ans = max(ans, 2);
-
-            cout << ans << " ";
+            cout << 2 << " ";
         }
 
         cout << "\n";
