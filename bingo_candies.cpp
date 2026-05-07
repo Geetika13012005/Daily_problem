c8a98a5d4f7d7d8e0f9e615ec80be6b94c376394
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-05-07 12:26:52 +0530

    bingo_candies

---
+++ bingo_candies.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        map<int, int> freq;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                int x;
+                cin >> x;
+                freq[x]++;
+            }
+        }
+
+        bool possible = true;
+
+        // If any color appears more than n*(n-1),
+        // then one full row/column of same color is unavoidable
+        for (auto &p : freq) {
+            if (p.second > n * (n - 1)) {
+                possible = false;
+                break;
+            }
+        }
+
+        cout << (possible ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
