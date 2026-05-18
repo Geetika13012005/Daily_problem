0b965776382cb9d8a05580f986f3b2b4f4c6ffb3
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-05-18 10:59:49 +0530

    palindrome

---
+++ palindromeSimple.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    string v = "aeiou";
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int base = n / 5;
+        int extra = n % 5;
+
+        string ans = "";
+
+        for (int i = 0; i < 5; i++) {
+            int cnt = base + (i < extra);
+
+            while (cnt--) {
+                ans += v[i];
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
