9b21847d8ef55efbccb1f1f3409b92ca900e6d1e
Geetika13012005 <geetika.cse-coend@bvp.edu.in>
2026-07-29 12:27:06 +0530

    update

--- makeItZero.cpp
+++ makeItZero.cpp
@@ -10,18 +10,12 @@ int main() {
         int n;
         cin >> n;
 
-        // We don't actually need the values.
         vector<int> a(n);
-        for (int i = 0; i < n; i++)
-            cin >> a[i];
-
-        // If already all zero, no operation is needed.
         bool allZero = true;
-        for (int x : a) {
-            if (x != 0) {
-                allZero = false;
-                break;
-            }
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            if (a[i] != 0) allZero = false;
         }
 
         if (allZero) {
@@ -30,45 +24,15 @@ int main() {
         }
 
         if (n % 2 == 0) {
-            // 6 operations
-            cout << 6 << "\n";
-            cout << 1 << " " << 2 << "\n";
-            cout << 3 << " " << 4 << "\n";
-
-            if (n > 4)
-                cout << 5 << " " << n << "\n";
-            else
-                cout << 3 << " " << 4 << "\n";
-
-            cout << 1 << " " << 3 << "\n";
-
-            if (n > 4)
-                cout << 4 << " " << n << "\n";
-            else
-                cout << 2 << " " << 4 << "\n";
-
-            cout << 1 << " " << n << "\n";
+            cout << 2 << "\n";
+            cout << "1 " << n << "\n";
+            cout << "1 " << n << "\n";
         } else {
-            // First solve first n-1 elements (even length),
-            // then use two more operations.
-            cout << 8 << "\n";
-            cout << 1 << " " << 2 << "\n";
-            cout << 3 << " " << 4 << "\n";
-
-            if (n - 1 > 4)
-                cout << 5 << " " << n - 1 << "\n";
-            else
-                cout << 3 << " " << 4 << "\n";
-
-            cout << 1 << " " << 3 << "\n";
-
-            if (n - 1 > 4)
-                cout << 4 << " " << n - 1 << "\n";
-            else
-                cout << 2 << " " << 4 << "\n";
-
-            cout << 2 << " " << n << "\n";
-            cout << 1 << " " << n << "\n";
+            cout << 4 << "\n";
+            cout << "1 2\n";
+            cout << "1 2\n";
+            cout << "2 " << n << "\n";
+            cout << "2 " << n << "\n";
         }
     }
 
